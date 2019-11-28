dec2fefc5d9ba85d3edf9f123ded7c50f1c29768
Antrn <2446026601@qq.com>
2019-11-28 21:42:15 +0800

    删除无用文件

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -13,7 +13,7 @@ Page({
   onLoad: function() {
     if (!wx.cloud) {
       wx.redirectTo({
-        url: '../chooseLib/chooseLib',
+        url: '../teamGo/teamGoIndex',
       })
       return
     }
@@ -54,15 +54,15 @@ Page({
       success: res => {
         console.log('[云函数] [login] user openid: ', res.result.openid)
         app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
+        // wx.navigateTo({
+        //   url: '../userConsole/userConsole',
+        // })
       },
       fail: err => {
         console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
+        // wx.navigateTo({
+        //   url: '../deployFunctions/deployFunctions',
+        // })
       }
     })
   },
